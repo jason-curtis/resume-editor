1a41476522bcfad20ff0b231d3c0997d4b631540
thatneat <thatneat@gmail.com>
2020-01-03 14:12:08 -0800

    Fix the tic-tac-toe game for Python 3, because that's important

--- tictactoe.py
+++ tictactoe.py
@@ -26,7 +26,7 @@ def algoReport(elapsed):
     global timeScoring
     global timeSymmetrizing
     totalEvals = sum(statesEvaluated)
-    print
+    print(
         "states pondered:",totalEvals,"(distribution:", statesEvaluated,\
         "\n  in", round(elapsed, 4), "seconds of CPU time",\
         "\n ",    round(elapsed/totalEvals,10),"secs/board",\
@@ -36,6 +36,7 @@ def algoReport(elapsed):
         "\n  (distribution:", statesSymmetrized,".",\
                     timeSymmetrizing, "seconds spent)",\
         "\n  alpha-beta pruning applied", sum(statesPruned),"times (distribution:", statesScored,")"
+    )
 
 def resetGlobals():
     global statesEvaluated
@@ -133,10 +134,10 @@ def boardPrint(board, showNums = 0):
                 strBoard += "\n-----\n"
             else:
                 strBoard += "|"
-    print strBoard
+    print(strBoard)
 
 def playMove(oldBoard, d, player, AIplayer):
-    print "\ncurrent player:", pName(player)
+    print("\ncurrent player:", pName(player))
     if d:
         if player == AIplayer:
             t1 = clock()#for elapsed time
@@ -150,15 +151,15 @@ def playMove(oldBoard, d, player, AIplayer):
             Move = askForMove(oldBoard)#get human player's decision
         
         newBoard = oldBoard[:Move] + [player] + oldBoard[Move+1:] #stick the x or o in place
-        print"taking square #", str(Move), ":"
+        print("taking square #", str(Move), ":")
         boardPrint(newBoard)
         
         s = score(newBoard, winCheck = 1)
         if s == 1:
-            print "x wins!"
+            print("x wins!")
             gameOver()
         elif s == -1:
-            print "o wins!"
+            print("o wins!")
             gameOver()
         else:
             playMove(newBoard, d-1, -player, AIplayer)
@@ -301,7 +302,7 @@ def chooseMove(oldBoard, d, player, boards = [], a = float("-infinity"), b = flo
     else: return (b,Move);
 
 def gameOver():
-    r = raw_input("GAME OVER! play again (y/n)? ")
+    r = input("GAME OVER! play again (y/n)? ")
     if r == "y":
         main()
 
@@ -315,21 +316,21 @@ def pName(p):# returns string version of player name
 
 def askForMove(board):#prompts human player to input move
     boardPrint(board, 1)
-    i = raw_input("Where would you like to move? ")
+    i = input("Where would you like to move? ")
     if i.isdigit():
         i = int(i)
     else:
-        print "Please enter a number on the board."
+        print("Please enter a number on the board.")
         return askForMove(board)#retry
         
     if -1<i and i<9 and not board[i]:
         return i
     else: 
-        print "sadface. you gotta gimme a number on the board that's not taken yet."
+        print("sadface. you gotta gimme a number on the board that's not taken yet.")
         return askForMove(board)#retry
 
 def askForAIPlayer():
-    p = raw_input("Which player gets AI (x/o/r for random)? ")
+    p = input("Which player gets AI (x/o/r for random)? ")
     
     if p == "x": AIplayer = 1 
     elif p == "o": AIplayer = -1
@@ -337,7 +338,7 @@ def askForAIPlayer():
         random.seed()
         AIplayer = random.choice([-1,1])
     else: 
-        print "unhappy face. you gotta answer the question with an x, o or r."
+        print("unhappy face. you gotta answer the question with an x, o or r.")
         return askForAIPlayer()#retry
     
     return AIplayer
@@ -347,7 +348,7 @@ def main():
 
     initBoard = [0,0,0,0,0,0,0,0,0]
     
-    print "AIplayer is", pName(AIplayer), "\nlet's get started!"
+    print("AIplayer is", pName(AIplayer), "\nlet's get started!")
     playMove(initBoard, 9, 1, AIplayer)
 
 main()
